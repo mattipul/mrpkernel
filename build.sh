d3e85c2caa87ab84fc3a21a8c4417f83feb285cd
Matti R Pulli <mattipul@svm-27.cs.helsinki.fi>
2014-01-01 17:05:37 +0200

    ...

--- build.sh
+++ build.sh
@@ -6,3 +6,10 @@ export PATH
 i586-elf-as ./src/boot.as -o ./compile/boot.o
 i586-elf-gcc -c ./src/kernel_main.c -o ./compile/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 i586-elf-gcc -T ./compile/linker.ld -o ./bin/kernel.bin -ffreestanding -O2 -nostdlib ./compile/boot.o ./compile/kernel.o
+
+mkdir -p isodir
+mkdir -p isodir/boot
+cp ./bin/kernel.bin isodir/boot/kernel.bin
+mkdir -p isodir/boot/grub
+cp ./compile/grub.cfg isodir/boot/grub/grub.cfg
+~/grub/bin/grub-mkrescue --xorriso="$HOME/xoriso/bin" -o kernel.iso isodir

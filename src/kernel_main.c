2800590e64c3d4e0c920b447d3406610e6ea8a35
mattipul <matti.pulli@helsinki.fi>
2014-01-04 20:09:40 +0200

    ..

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,5 +1,8 @@
 #include "kernel_debug.h"
 
+#if defined(__cplusplus)
+extern "C" /* Use C linkage for kernel_main. */
+#endif
 void kernel_main(){
 	kernel_debug_init();
 	kernel_debug_putchar('M');

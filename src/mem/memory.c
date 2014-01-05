bd3a6debb4a626afe84428d5d1c4a240b5419b62
mattipul <matti.pulli@helsinki.fi>
2014-01-05 13:46:35 +0200

    ..

--- src/mem/memory.c
+++ src/mem/memory.c
@@ -4,7 +4,7 @@ int memory_init(){
 	memory_detect();
 }
 
-int memory_detect()[
+int memory_detect(){
 	kernel_smap=(SMAP_entry_t*) 0x1000;
 	const int smap_size = 0x2000;
 	int entry_count = detectMemory(kernel_smap, smap_size / sizeof(SMAP_entry_t));

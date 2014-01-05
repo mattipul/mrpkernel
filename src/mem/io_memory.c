40a74c9702f6f33b4175b7096e464c3266290235
mattipul <matti.pulli@helsinki.fi>
2014-01-05 10:38:26 +0200

    ..

--- src/mem/io_memory.c
+++ src/mem/io_memory.c
@@ -1,16 +1,16 @@
 #include "io_memory.h"
 
-void io_memory_put_into_memory(void* address, unsigned short int data){
-	unsigned short int *mem_address=(unsigned short int*)(*address);
+void io_memory_put_into_memory(char address, unsigned short int data){
+	unsigned short int *mem_address=(unsigned short int*)address;
 	mem_address[0]=data;
 }
 
-unsigned short int io_memory_get_from_memory(void* address){
-	unsigned short int *mem_address=(unsigned short int*)(*address);
+unsigned short int io_memory_get_from_memory(char address){
+	unsigned short int *mem_address=(unsigned short int*)address;
 	return mem_address[0];
 }
 
-void io_memory_move_in_memory(void* address_from, void* address_to){
-	unsigned short int mem_data=io_memory_get_from_memory(*address_from);
-	io_memory_put_into_memory(*address_to, mem_data);
+void io_memory_move_in_memory(char address_from, char address_to){
+	unsigned short int mem_data=io_memory_get_from_memory(address_from);
+	io_memory_put_into_memory(address_to, mem_data);
 }

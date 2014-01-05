b336c8ad6c366734f7ae773a69140aa9726bac4a
mattipul <matti.pulli@helsinki.fi>
2014-01-05 10:41:12 +0200

    ..

--- src/mem/io_memory.h
+++ src/mem/io_memory.h
@@ -2,7 +2,7 @@
 #define IO_MEMORY_H_GUARD
 
 void io_memory_put_into_memory(unsigned short int address, unsigned short int data);
-void io_memory_get_from_memory(unsigned short int address);
+unsigned short int io_memory_get_from_memory(unsigned short int address);
 void io_memory_move_in_memory(unsigned short int address_from, unsigned short int address_to);
 
 #endif

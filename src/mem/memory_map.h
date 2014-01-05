d3b5c99eb9bdff25fd3e3a4d3e94d5c2731c1d11
mattipul <matti.pulli@helsinki.fi>
2014-01-05 13:41:29 +0200

    ..

--- src/mem/memory_map.h
+++ src/mem/memory_map.h
@@ -2,7 +2,6 @@
 #define MEMORY_MAP_H_GUARD
 
 #include <stdint.h>
-#include "../archives/memory_map_structure.h"
 
 int __attribute__((noinline)) __attribute__((regparm(3))) detectMemory(SMAP_entry_t* buffer, int maxentries);
 

376edae8e8c8258be8014b3948975b4115c67005
mattipul <matti.pulli@helsinki.fi>
2014-01-05 10:30:32 +0200

    ..

--- src/mem/io_memory.h
+++ src/mem/io_memory.h
@@ -1,8 +1,8 @@
 #ifndef IO_MEMORY_H_GUARD
 #define IO_MEMORY_H_GUARD
 
-void io_memory_put_into_memory(unsigned short int address, unsigned short int data);
-void io_memory_get_from_memory(unsigned short int address);
-void io_memory_move_in_memory(unsigned short int address_from, unsigned short int address_to);
+void io_memory_put_into_memory(void address, unsigned short int data);
+void io_memory_get_from_memory(void address);
+void io_memory_move_in_memory(void address_from, void address_to);
 
 #endif

52a460128b0a0ef26b6b8d9344ca9cf847d17bb0
mattipul <matti.pulli@helsinki.fi>
2014-01-05 10:40:24 +0200

    ..

--- src/mem/io_memory.c
+++ src/mem/io_memory.c
@@ -1,16 +1,16 @@
 #include "io_memory.h"
 
-void io_memory_put_into_memory(char address, unsigned short int data){
+void io_memory_put_into_memory(unsigned short int address, unsigned short int data){
 	unsigned short int *mem_address=(unsigned short int*)address;
 	mem_address[0]=data;
 }
 
-unsigned short int io_memory_get_from_memory(char address){
+unsigned short int io_memory_get_from_memory(unsigned short int address){
 	unsigned short int *mem_address=(unsigned short int*)address;
 	return mem_address[0];
 }
 
-void io_memory_move_in_memory(char address_from, char address_to){
+void io_memory_move_in_memory(unsigned short int address_from, unsigned short int address_to){
 	unsigned short int mem_data=io_memory_get_from_memory(address_from);
 	io_memory_put_into_memory(address_to, mem_data);
 }
--- src/mem/io_memory.h
+++ src/mem/io_memory.h
@@ -1,8 +1,8 @@
 #ifndef IO_MEMORY_H_GUARD
 #define IO_MEMORY_H_GUARD
 
-void io_memory_put_into_memory(char address, unsigned short int data);
-void io_memory_get_from_memory(char address);
-void io_memory_move_in_memory(char address_from, char address_to);
+void io_memory_put_into_memory(unsigned short int address, unsigned short int data);
+void io_memory_get_from_memory(unsigned short int address);
+void io_memory_move_in_memory(unsigned short int address_from, unsigned short int address_to);
 
 #endif

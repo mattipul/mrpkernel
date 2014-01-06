e84ff70ec9f222ff6f7797ab73eca6fad8e74f71
mattipul <matti.pulli@helsinki.fi>
2014-01-06 21:15:24 +0200

    ...

--- src/mem/memory.c
+++ src/mem/memory.c
@@ -9,13 +9,11 @@ void memory_detect(){
 
 
 	SMAP_entry_t* smap = (SMAP_entry_t*) 0x1000;
-	const int smap_size = 0x2000;
+	int entry_count=detectMemory(smap);
  
-	int entry_count=detectMemory(smap, smap_size / sizeof(SMAP_entry_t));
- 
-	if (entry_count == -1) {
+	/*if (entry_count == -1) {
 		kernel_debug_putchar('A');
 	} else {
 		kernel_debug_putchar('B');
-	}
+	}*/
 }
--- src/mem/memory_map.c
+++ src/mem/memory_map.c
@@ -2,20 +2,17 @@ __asm__(".code16gcc\n");
 
 #include "memory_map.h"
 
-
-
 int detectMemory(SMAP_entry_t* buffer, int maxentries)
 {
 	uint32_t contID = 0;
 	int entries = 0, signature, bytes;
-	int i;
-	//for(i=0; i<2; i++)
-	//{
-		__asm__ __volatile__ ("int  $0x15" 
+
+	__asm__ __volatile__ ("int  $0x15" 
 				: "=a"(signature), "=c"(bytes), "=b"(contID)
 				: "a"(0xE820), "b"(contID), "c"(24), "d"(0x534D4150), "D"(buffer));
-		if (signature != 0x534D4150) 
+		if (signature != 0x534D4150) {
 			return -1; // error
+		}
 		if (bytes > 20 && (buffer->ACPI & 0x0001) == 0)
 		{
 			// ignore this entry
@@ -24,11 +21,6 @@ int detectMemory(SMAP_entry_t* buffer, int maxentries)
 			buffer++;
 			entries++;
 		}
-		
-	//	if(contID==0){
-	//		break;
-	//	}
-	//} 
-	//while (contID != 0 && entries < 1);	                                                                 
+                                                               
 	return entries;
 }

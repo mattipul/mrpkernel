76ba3f7604a31d9455c38a66dbc6ab3e8696ada9
Matti R Pulli <mattipul@svm-27.cs.helsinki.fi>
2014-01-06 20:25:19 +0200

    --

--- src/mem/memory.c
+++ src/mem/memory.c
@@ -9,7 +9,7 @@ void memory_detect(){
 	SMAP_entry_t* smap = (SMAP_entry_t*) 0x1000;
 	const int smap_size = 0x2000;
  
-	detectMemory(smap, smap_size / sizeof(SMAP_entry_t));
+//	detectMemory(smap, smap_size / sizeof(SMAP_entry_t));
  
 //	if (entry_count == -1) {
 //	} else {

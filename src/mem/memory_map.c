2933a62937a02259569ffe185090d0f7ce0c8b9f
Matti R Pulli <mattipul@svm-27.cs.helsinki.fi>
2014-01-06 20:53:46 +0200

    ...

--- src/mem/memory_map.c
+++ src/mem/memory_map.c
@@ -8,13 +8,13 @@ void detectMemory(SMAP_entry_t* buffer, int maxentries)
 {
 	uint32_t contID = 0;
 	int entries = 0, signature, bytes;
-	do 
-	{
+	//do 
+	//{
 		__asm__ __volatile__ ("int  $0x15" 
 				: "=a"(signature), "=c"(bytes), "=b"(contID)
 				: "a"(0xE820), "b"(contID), "c"(24), "d"(0x534D4150), "D"(buffer));
 		if (signature != 0x534D4150) 
-			return -1; // error
+			//return -1; // error
 		if (bytes > 20 && (buffer->ACPI & 0x0001) == 0)
 		{
 			// ignore this entry
@@ -23,7 +23,7 @@ void detectMemory(SMAP_entry_t* buffer, int maxentries)
 			buffer++;
 			entries++;
 		}
-	} 
-	while (contID != 0 && entries < maxentries);	                                                                 
+	//} 
+	//while (contID != 0 && entries < maxentries);	                                                                 
 //	return entries;
 }

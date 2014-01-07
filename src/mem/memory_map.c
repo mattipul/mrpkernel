8fb31df137f8b6dfa193b87de5036dfbec729c1e
Matti R Pulli <mattipul@svm-27.cs.helsinki.fi>
2014-01-07 18:14:51 +0200

    ...

--- src/mem/memory_map.c
+++ src/mem/memory_map.c
@@ -1,17 +1,11 @@
 #include "memory_map.h"
 
 void memory_map_detect_memory(multiboot_info_t* mbd){
-	multiboot_memory_map_t* mmap = mbd->mmap_addr;
-//	kernel_debug_putchar('A');
-//	kernel_debug_putchar('0'+(char)mmap->type);
-	
-	kernel_debug_putchar('0'+mbd->mmap_length);		
-	
+	multiboot_memory_map_t* mmap = mbd->mmap_addr;	
 	while(mmap < mbd->mmap_addr + mbd->mmap_length) {
-	//	kernel_debug_putchar('P');
+		kernel_debug_putchar('0'+mmap->type);
 		mmap = (multiboot_memory_map_t*) ( (unsigned int)mmap + mmap->size + sizeof(unsigned int) );
 	}
-	//kernel_debug_putchar('B');
 }
 
 

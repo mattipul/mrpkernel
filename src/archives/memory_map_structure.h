3387da2256f96681c12d6ae4a9a6db5f4925f40a
mattipul <matti.pulli@helsinki.fi>
2014-01-07 16:49:51 +0200

    ---

--- src/archives/memory_map_structure.h
+++ src/archives/memory_map_structure.h
@@ -3,4 +3,11 @@
 
 #include <stdint.h>
 
+typedef struct multiboot_memory_map {
+	unsigned int size;
+	unsigned int base_addr_low,base_addr_high;
+	unsigned int length_low,length_high;
+	unsigned int type;
+} multiboot_memory_map_t;
+
 #endif

cfb9fe302c076e31e7a31bbb3c14ae730a40dd76
Matti R Pulli <mattipul@svm-27.cs.helsinki.fi>
2014-01-18 20:44:18 +0200

    ..

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -23,11 +23,11 @@ void kernel_init(){
 
 	struct PROCESS_PCB *curpcb=process_create();
 	uint16_t *prc_addr=curpcb->segment->base_address;
-	prc_addr[0]=&test_process_1;
+	//prc_addr[0]=&test_process_1;
 	kernel_debug_binary(curpcb);
 	kernel_debug_yplus();
 	scheduler_enqueue(curpcb);
-	exec_process(prc_addr[0]);
+	exec_process(test_process_1);
 	//kernel_debug_binary((uint16_t)prc_addr[0][0]);
 	//kernel_run();
 }

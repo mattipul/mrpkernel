c1e50d095f9008339d83d4b7e21bb74146648e7a
Matti R Pulli <mattipul@svm-27.cs.helsinki.fi>
2014-01-18 21:57:54 +0200

    ..

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -31,6 +31,8 @@ void kernel_init(){
 	void (*proc)();
 	//proc=&prc_addr[0];
 	//proc();
+	kernel_debug_binary(&test_process_1);
+	kernel_debug_yplus();
 	kernel_debug_binary(prc_addr[0]);
 	//kernel_debug_binary((uint16_t)prc_addr[0][0]);
 	//kernel_run();

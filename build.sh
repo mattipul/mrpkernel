625e12e766a765b279c121dc7a6edf60bbee53e5
mattipul <matti.pulli@helsinki.fi>
2014-01-18 15:12:11 +0200

    1.

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ i586-elf-as ./src/boot.as -o ./compile/boot.o
 
 i586-elf-gcc -c ./src/kernel/kernel.c -o ./compile/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -I/usr/include -w
 
+i586-elf-gcc -c ./src/scheduler/scheduler.c -o ./compile/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -I/usr/include -w
+
 i586-elf-gcc -c ./src/memory/memory.c -o ./compile/memory.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -I/usr/include -w
 
 i586-elf-gcc -c ./src/irq/irq.c -o ./compile/irq.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -I/usr/include -w
@@ -19,4 +21,4 @@ i586-elf-gcc -c ./src/debug/kernel_debug.c -o ./compile/kernel_debug.o -std=gnu9
 
 i586-elf-gcc -c ./src/kernel_main.c -o ./compile/kernel_main.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -I/usr/include -w
 
-i586-elf-gcc -T ./compile/linker.ld -o ./bin/kernel.bin -ffreestanding -O2 -nostdlib ./compile/boot.o ./compile/kernel_main.o ./compile/kernel_debug.o ./compile/memory.o ./compile/kernel.o ./compile/irq.o ./compile/process.o 
+i586-elf-gcc -T ./compile/linker.ld -o ./bin/kernel.bin -ffreestanding -O2 -nostdlib ./compile/boot.o ./compile/kernel_main.o ./compile/kernel_debug.o ./compile/memory.o ./compile/kernel.o ./compile/irq.o ./compile/process.o ./compile/scheduler.o

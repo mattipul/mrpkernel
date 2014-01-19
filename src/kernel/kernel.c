95ded57c85086affbaf7caa8211473778fee4869
Matti R Pulli <mattipul@svm-27.cs.helsinki.fi>
2014-01-19 14:36:52 +0200

    ..

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -21,7 +21,7 @@ void kernel_init(){
 	asm volatile ( "pushf\n\t"
 	               "popl %0"
 	             : "=g"(f) );
-	kernel_debug_binary(f & ( 1 << 9 ));
+	kernel_debug_binary((uint16_t)f & (uint16_t)( 1 << 9 ));
 	
 	pid_counter=0;
 	memory_stack_pointer=MEMORY_BLOCK_START;

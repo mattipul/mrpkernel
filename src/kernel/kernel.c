8a15f020d88d9dd97c308b62610a4263291f8097
Matti R Pulli <mattipul@svm-27.cs.helsinki.fi>
2014-01-19 13:15:45 +0200

    ...

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -15,7 +15,9 @@ void process_2(){
 void kernel_init(){
 	kernel_debug_init();
 	irq_init();
-	asm("int $0"::"r"(0x00):);
+
+	asm volatile("INT $0x00");
+	
 	pid_counter=0;
 	memory_stack_pointer=MEMORY_BLOCK_START;
 	process_counter=0;
